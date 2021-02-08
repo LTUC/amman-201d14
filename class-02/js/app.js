b70dc1a7b9bb8ff13d97eb3808c5baded72d5ef7
bashar-alasmar <basharalasmar96@gmail.com>
2021-02-08 12:59:42 +0200

    class-02

--- class-02/js/app.js
+++ class-02/js/app.js
@@ -1,35 +1,34 @@
 'use strict';
 
-// let confirmUser = confirm('do you like movies?');
-// console.log(confirmUser);
-// // ok 
-// // cancel 
-
-// if (confirmUser){ // 
-//   alert('welcome to the movies page');
-// }
-// else{
-//   alert('omg');
-// }
-// let myBool = false;
-// let anotherBool = false;
-// let thirdBool = true;
+let confirmUser = confirm('do you like movies?');
+console.log(confirmUser);
 
 
-// if (myBool && anotherBool) { // false
-//   console.log('myBool and thirdBool were both true');
-// } // only one of these have to be true
-// else if (myBool || anotherBool) { // false
-//   console.log('My bool or anotherBool was true!');
-// } // only thirdBool can be true
-// else if (myBool) { // F
-//   console.log('myBool and anotherBool were false, but thirdBool was true.');
-// } // none of the above conditions were true
-// else { // default case
-//   console.log('Were any of my variables true?');
-// }
+if (confirmUser){ // 
+  alert('welcome to the movies page');
+}
+else{
+  alert('omg');
+}
+let myBool = false;
+let anotherBool = false;
+let thirdBool = true;
 
-//parseInt
+
+if (myBool && anotherBool) { // false
+  console.log('myBool and thirdBool were both true');
+} // only one of these have to be true
+else if (myBool || anotherBool) { // false
+  console.log('My bool or anotherBool was true!');
+} // only thirdBool can be true
+else if (myBool) { // F
+  console.log('myBool and anotherBool were false, but thirdBool was true.');
+} // none of the above conditions were true
+else { // default case
+  console.log('Were any of my variables true?');
+}
+
+
 let myCharacter = 'invoker';
 let userFav = prompt('what is your fav character?');
 // console.log(userFav.toLowerCase()); // invoker // the way the user write it.
@@ -45,5 +44,11 @@ console.log(userFav);
 
 
 
-let userinput = prompt('do you like paris');
+let userinput = prompt('do you think I like paris');
+if (userinput ==='yes' || userinput ==='y'){
+alert('you got it right !')
+} else if(userinput ==='no' || userinput ==='n'){
+  alert('no actually i love it ')
+}
+ 
 
