b7af492ceac525bee684e52cd56a0c11dbbbb1a3
bashar-alasmar <basharalasmar96@gmail.com>
2021-02-10 16:04:12 +0200

    demo

---
+++ class-04/demo/js/app.js
@@ -0,0 +1,112 @@
+'use strict';
+
+// let x =  10;
+
+// while(x === 40){ // evalutes to true of false
+//     console.log(x);
+//     x++;
+// }
+
+// let i = 10;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i === 40) 
+
+// the main difference between both loops is at least we're going to excute the
+// do while 
+
+
+// Functions : block of code doing a certin does something(task)
+// organize the code , structuring the code  
+// reusable 
+// return some value 
+
+// scopes (global), (Local);
+
+
+// declration function 
+// function name(){
+    // code block
+//} 
+// name();
+
+// let userName = 'Mohammad'; // globally defined
+// // console.log('Global variable',userName)
+// let password= 99; // global variable
+
+
+// function welcomingMessage(userName,lastname){ // <-- parameter
+//     let userInfo = 'Welcome to my webPage ' + userName + ' '+ lastname;
+//     return userInfo
+// }
+
+
+
+// console.log(welcomingMessage('Bashar' , 'Alasmar')); // <-- argument
+
+// console.log(password);
+
+// function square(length){
+//     let area = length * length;
+
+//     return area
+// }
+
+
+
+// function sumAndMultiply(x,y){
+//     let summation = x + y;
+//     let muliplication = x * y; 
+//     console.log('Code Fellows')
+//     // let division = x / y; 
+//     // let reminder = x % y;
+//     // console.log('ASAC');
+
+//     return [summation, muliplication]
+// }
+// // console.log(sumAndMultiply(10,9));
+// // let values = sumAndMultiply(10,9);
+
+// function muliplicationOfSummation(){
+//     console.log('ASAC')
+//       let num1 =  sumAndMultiply(15,7)[0];
+//       let num2 = sumAndMultiply(3,6)[0];
+
+//       let multply = num1 * num2;
+//       return multply
+// }
+
+
+// console.log(muliplicationOfSummation());
+
+
+// for(let i = 0 ; i < 3 ; i++){
+//     console.log('this is a function declration',getAge(2000));
+// }
+// function expression 
+
+// console.log('this is a function declration',getAge(2000));
+// function getAge (year){
+//     let age = 2021 -year
+//     return age;
+// }
+
+
+
+// console.log(movieName);
+
+// let movieName = 'Fast&furios';
+
+
+
+// let getBirthDate = function(year){ // annoymus function
+//     let age = 2021 - year;
+//     return age;
+// }
+// console.log('this is a function expression',getBirthDate(1996))
+
+// console.log(getBirthDate(1998))
+// console.log(getBirthDate(1994))
+// console.log(getBirthDate(1980))
+
