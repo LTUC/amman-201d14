08c76442e6179121ccc359d57a4a20db46efd832
bashar-alasmar <basharalasmar96@gmail.com>
2021-02-15 13:11:50 +0200

    pushindDemo

---
+++ class-07/codeReview/app.js
@@ -0,0 +1,116 @@
+'use strict';
+              // 9   // 8  
+let hours = ['6am','7pm','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+let container = document.getElementById('container');
+// let table = document.createElement('table');
+
+
+let seattle = {
+    location:'Seattle',
+    minCustomer:23,
+    maxCustomer:65,
+    avgPerSale:6.3,
+    randomCustomers:[],
+    numOfCookiesSoldPerHour: [],
+    total:0,
+    randomCustomerPerHour:function(){
+        for(let i = 0 ; i < hours.length ; i++){
+            this.randomCustomers.push(randomGenrator(this.minCustomer,this.maxCustomer));
+        }
+    },
+    cookiesSoldPerHour:function(){
+        for(let i = 0 ; i < hours.length; i++){
+        this.numOfCookiesSoldPerHour.push(Math.ceil(this.randomCustomers[i]*this.avgPerSale));
+        this.total = this.total + this.numOfCookiesSoldPerHour[i];
+        // undifined = undifined + 35 
+        }
+
+    },
+    render:function(){
+        let headerEl = document.createElement('h2');
+        container.appendChild(headerEl);
+        headerEl.textContent = this.location;
+        let ulEl = document.createElement('ul');
+        container.appendChild(ulEl);
+        let liEl;
+        for(let i =0 ; i<this.numOfCookiesSoldPerHour.length; i++){
+            liEl = document.createElement('li');
+            ulEl.appendChild(liEl);
+            liEl.textContent = `${hours[i]} : ${this.numOfCookiesSoldPerHour[i]} Cookies`;
+        }
+        liEl = document.createElement('li');
+        ulEl.appendChild(liEl);
+        liEl.textContent =  'Total : ' + this.total + ' Cookies';
+
+    }
+}
+seattle.randomCustomerPerHour();
+seattle.cookiesSoldPerHour();
+seattle.render();
+
+
+
+let tokyo = {
+    location:'Tokyo',
+    minCustomer:3,
+    maxCustomer:24,
+    avgPerSale:1.2,
+    randomCustomers:[],
+    numOfCookiesSoldPerHour: [],
+    total:0,
+    randomCustomerPerHour:function(){
+        for(let i = 0 ; i < hours.length ; i++){
+            this.randomCustomers.push(randomGenrator(this.minCustomer,this.maxCustomer));
+        }
+    },
+    cookiesSoldPerHour:function(){
+        for(let i = 0 ; i < hours.length; i++){
+        this.numOfCookiesSoldPerHour.push(Math.ceil(this.randomCustomers[i]*this.avgPerSale));
+        this.total = this.total + this.numOfCookiesSoldPerHour[i];
+        // undifined = undifined + 35 
+        }
+
+    },
+    render:function(){
+        let headerEl = document.createElement('h2');
+        container.appendChild(headerEl);
+        headerEl.textContent = this.location;
+        let ulEl = document.createElement('ul');
+        container.appendChild(ulEl);
+        let liEl;
+        for(let i =0 ; i<this.numOfCookiesSoldPerHour.length; i++){
+            liEl = document.createElement('li');
+            ulEl.appendChild(liEl);
+            liEl.textContent = `${hours[i]} : ${this.numOfCookiesSoldPerHour[i]} Cookies`;
+        }
+        liEl = document.createElement('li');
+        ulEl.appendChild(liEl);
+        liEl.textContent =  'Total : ' + this.total + ' Cookies';
+
+    }
+}
+tokyo.randomCustomerPerHour();
+tokyo.cookiesSoldPerHour();
+tokyo.render();
+
+
+function randomGenrator(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  }
+
+
+// 1 TABLE 
+//   function headerRow(){
+//     // first row 
+//   }
+
+// Location.prototype.render = function(){
+//     //the table data body
+      // seattle 1,1,1,1,1,10
+      // tokyo 2,3,5,8,9,10
+
+// }
+   
+//   function footerRow(){
+//    // last row 
+//   }
---
+++ class-07/kittenDemo/js/app.js
@@ -0,0 +1,82 @@
+'use strict';
+function randomValue(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+                                        //true
+function Kitten(kittenName,likes,isGoodWithDogs,isGoodWithKids,isGoodwithOtherCats){
+this.name = kittenName;
+this.age = 0;
+this.likes = likes;
+this.isGoodWithDogs = isGoodWithDogs;
+this.isGoodWithKids = isGoodWithKids;
+this.isGoodwithOtherCats = isGoodwithOtherCats;
+}
+Kitten.prototype.updateAge = function(){
+    this.age = randomValue(3,12) + ' months';
+}
+Kitten.prototype.render = function(){
+
+        let divEl = document.getElementById('kittenProfiles');
+        let articleEl = document.createElement('article');
+        divEl.appendChild(articleEl);
+        let header2 = document.createElement('h2');
+        articleEl.appendChild(header2);
+        header2.textContent = this.name;
+        let paragraphEl = document.createElement('p');
+        articleEl.appendChild(paragraphEl);
+        paragraphEl.textContent = `${this.name} is actually really fun cat`;
+        let ulEl = document.createElement('ul');
+        articleEl.appendChild(ulEl);
+        // each time im creating a list item
+        let liEl; 
+        for(let i = 0 ; i <this.likes.length; i++)
+        {
+            liEl = document.createElement('li');
+            ulEl.appendChild(liEl);
+            liEl.textContent = this.likes[i];
+        }
+
+        let imageEl = document.createElement('img');
+        articleEl.appendChild(imageEl);
+        imageEl.setAttribute('src',`images/${this.name}.jpeg`)
+
+        // imageEl.setAttribute('alt','Some text generated')
+        let table = document.createElement('table');
+        articleEl.appendChild(table);
+
+        let tr = document.createElement('tr');
+        table.appendChild(tr);
+        let th1 = document.createElement('th');
+        tr.appendChild(th1);
+        th1.textContent = `IsGoodWithDogs`;
+        let th2 = document.createElement('th');
+        tr.appendChild(th2);
+        th2.textContent = `isGoodWithKids`;
+        let th3 = document.createElement('th');
+        tr.appendChild(th3);
+        th3.textContent = `isGoodwithOtherCats`;
+        let tr2 = document.createElement('tr');
+        table.appendChild(tr2);
+        let td1 = document.createElement('td');
+        tr2.appendChild(td1);
+        td1.textContent = this.isGoodWithDogs;
+        let td2 = document.createElement('td');
+        tr2.appendChild(td2);
+        td2.textContent = this.isGoodWithKids;
+        let td3 = document.createElement('td');
+        tr2.appendChild(td3);
+        td3.textContent = this.isGoodwithOtherCats;
+    }
+
+// Prototype object
+
+let frankie = new Kitten('frankie', ['playing','eating','jumping'],true, false,false);
+frankie.updateAge();
+frankie.render();
+// console.log(frankie);
+let serena = new Kitten('serena', ['shopping','playingAround','running'],false,false,false);
+serena.updateAge();
+serena.render();
+let jumper = new Kitten('jumper',['jumping','eating','sleeping'],true,true,true);
+jumper.updateAge();
+jumper.render();
